8109b65ca4050c313d817dcd049f0e5b1affe170
Casper Ståhl <casperstaahl+gh@gmail.com>
2025-08-12 17:02:55 +0200

    Update GSemSS

--- theories/graphsem.v
+++ theories/graphsem.v
@@ -16,13 +16,16 @@ From Lagois Require Import lagoisgraph.
 (* From Coq Require Import Logic.EqdepFacts. *)
 (* Require Import Coq.Arith.Wf_nat. *)
 
+Section abstract_syntax.
 
-Definition Var := nat.
-Definition Ch := nat.
+Context
+  (G : LagoisGraph.type)
+  (Var : finType).
+
 Definition Val := nat.
 
 (* Abstract syntax of expressions *)
-Inductive Exp : Set :=
+Inductive Exp : Type :=
   | const : Val -> Exp
   | var : Var -> Exp
   | add : Exp -> Exp -> Exp
@@ -32,7 +35,7 @@ Inductive Exp : Set :=
 
 (* Definition of m(e) *)
 Definition Mem := Var -> Val.
-Fixpoint ExpSem (m : Mem) (e : Exp)  : Val :=
+Fixpoint ExpSem (m : Mem) (e : Exp) : Val :=
   match e with
   | const n => n
   | var v => m v
@@ -41,77 +44,111 @@ Fixpoint ExpSem (m : Mem) (e : Exp)  : Val :=
   | not e' => if 0 < ExpSem m e' then 0 else 1
   | cmp e1 e2 => ExpSem m e1 == ExpSem m e2
   end.
+Notation "m <( e )>" := (ExpSem m e) (at level 20).
 
 (* Abstract syntax of statements *)
-Inductive Stmt : Set :=
+Inductive Stmt {v : G} : Type :=
   | skip : Stmt
   | assign : Var -> Exp -> Stmt
-  | output : Ch -> Exp -> Stmt
-  | input : Var -> Ch -> Stmt
+  | putbuf : L(v) -> Exp -> Stmt
+  | getbuf : L(v) -> Var -> Stmt
+  | exch (v' : G) (f : v @> v') : L(v) -> Stmt
   | ifelse : Exp -> Stmt -> Stmt -> Stmt
   | while : Exp -> Stmt -> Stmt
   | seque : Stmt -> Stmt -> Stmt.
 
-Definition State := (Stmt * Mem)%type.
+Definition Buf := seq Val.
+Definition BufM (v : G) := L(v) -> Buf.
+Definition State v := (@Stmt v * Mem * BufM v)%type.
 
 (* Definition of signals *)
-Inductive Sig : Type :=
+Inductive Sig {v : G} : Type :=
   | ε_sig : Sig
-  | ass_sig : Var -> Sig
-  | out_sig : Ch -> Val -> Sig
-  | in_sig : Ch -> Var -> Val -> Sig.
+  | putbuf_sig : L(v) -> Val -> Sig
+  | getbuf_sig : L(v) -> Val -> Sig
+  | exch_sig (v' : G) : L(v) -> L(v') -> Val -> Sig.
 
 (* Small steps semantics of local execution *)
-Inductive SemSS : State -> Sig -> State -> Prop :=
-  | ex_assign m x e n :
-      ExpSem m e = n ->
-      SemSS (assign x e, m) (ass_sig x)
-          (skip, fun_of_simpl [eta m with x |-> n])
-  | ex_out m c e n:
-      ExpSem m e = n ->
-      SemSS (output c e, m) (out_sig c n) (skip, m)
-  | ex_in m c x n :
-      SemSS (input c x, m) (in_sig c x n)
-          (skip, fun_of_simpl [eta m with x |-> n])
-  | ex_ifelse_ff e s1 s2 m :
-      ExpSem m e = 0 ->
-      SemSS (ifelse e s1 s2, m) ε_sig (s2, m)
-  | ex_ifelse_tt e s1 s2 m :
+Inductive SemSS {v : G} : @Sig v -> State v -> State v -> Prop :=
+  | ex_assign m b x e n :
+      ExpSem m e == n ->
+      SemSS ε_sig
+          (assign x e, m, b)
+          (skip, [eta m with x |-> n] : Mem, b)
+  | ex_putbuf m b p e n :
+      ExpSem m e == n ->
+      SemSS (putbuf_sig p n)
+          (putbuf p e, m, b)
+          (skip, m, [eta b with p |-> b p ++ [:: n]] : BufM v)
+  | ex_getbuf m b p x n :
+      ohead (b p) == Some (n) ->
+      SemSS (getbuf_sig p n)
+          (getbuf p x, m, b)
+          (skip, [eta m with x |-> n] : Mem, [eta b with p |-> behead (b p)] : BufM v)
+  | ex_exch m b v' (f : v @> v') p n :
+      ohead (b p) == Some (n) ->
+      SemSS (exch_sig p (f p) n)
+          (exch f p, m, b)
+          (skip, m, [eta b with p |-> behead (b p)] : BufM v)
+  | ex_ifelse_ff m b e s1 s2 :
+      ExpSem m e == 0 ->
+      SemSS ε_sig
+          (ifelse e s1 s2, m, b)
+          (s2, m, b)
+  | ex_ifelse_tt m b e s1 s2 :
       0 < ExpSem m e ->
-      SemSS (ifelse e s1 s2, m) ε_sig (s1, m)
-  | ex_while e s m :
-      SemSS (while e s, m) ε_sig (ifelse e (seque s (while e s)) skip, m)
-  | ex_seque_cont s1 s1' s2 m m' ϕ :
-      SemSS (s1, m) ϕ (s1', m') ->
-      SemSS (seque s1 s2, m) ϕ (seque s1' s2, m')
-  | ex_seque_skip s m :
-      SemSS (seque skip s, m) ε_sig (s, m).
+      SemSS ε_sig
+          (ifelse e s1 s2, m, b)
+          (s1, m, b)
+  | ex_while m b e s :
+      SemSS ε_sig
+          (while e s, m, b)
+          (ifelse e (seque s (while e s)) skip, m, b)
+  | ex_seque_cont m m' b b' s1 s1' s2 ϕ :
+      SemSS ϕ (s1, m, b) (s1', m', b') ->
+      SemSS ϕ (seque s1 s2, m, b) (seque s1' s2, m', b')
+  | ex_seque_skip m b s :
+      SemSS ε_sig (seque skip s, m, b) (s, m, b).
 
 (* Definition of events *)
-Inductive Ev G : Type :=
-  | ε_ev : Ev G
-  | ass_ev : G -> Var -> Ev G.
-Arguments ε_ev {G}.
+Inductive Ev : Type :=
+  | ε_ev : Ev
+  | putbuf_ev (v : G) : L(v) -> Val -> Ev
+  | getbuf_ev (v : G) : L(v) -> Val -> Ev
+  | exch_ev (v v' : G) : L(v) -> L(v') -> Val -> Ev.
 
-Definition GState (G : LagoisGraph.type) := G -> State.
+Definition GState := forall (v : G), State v.
+
+Definition update_GState (St : GState) (v : G) (st : State v): GState.
+Proof.
+  move=> v'.
+  case v_eq_v': (v == v').
+    move/eqP in v_eq_v'.
+    elim: v_eq_v'.
+    exact: st.
+  exact: (St v').
+Defined.
 
 (* Small steps semantics of distributed execution *)
-Inductive GSemSS G : GState G -> Ev G -> GState G -> Prop :=
-  | ex_sp St v st st':
+Inductive GSemSS : Ev -> GState -> GState -> Prop :=
+  | Ex_sp St v st st':
       St v = st ->
-      SemSS st ε_sig st' ->
-      GSemSS St ε_ev [eta St with v |-> st']
-  | ex_lp St v st st' x :
+      SemSS ε_sig st st' ->
+      GSemSS ε_ev St (@update_GState St v st')
+  | Ex_putbuf St v st st' (p : L(v)) n :
       St v = st ->
-      SemSS st (ass_sig x) st' ->
-      GSemSS St (ass_ev v x) [eta St with v |-> st']
-  | ex_exch St v_i v_j st_i st'_i st_j st'_j c x n:
-      v_i @> v_j ->
-      St v_i = st_i ->
-      St v_j = st_j ->
-      SemSS st_i (out_sig c n) st'_i ->
-      SemSS st_j (in_sig c x n) st'_j ->
-      GSemSS St (ass_ev v_j x) [eta St with v_i |-> st'_i, v_j |-> st'_j].
+      SemSS (putbuf_sig p n) st st' ->
+      GSemSS (putbuf_ev p n) St (@update_GState St v st')
+  | Ex_getbuf St v st st' (p : L(v)) n :
+      St v = st ->
+      SemSS (getbuf_sig p n) st st' ->
+      GSemSS (getbuf_ev p n) St (@update_GState St v st')
+  | Ex_exch St v1 v2 st1 st1' st2 st2' (p : L(v1)) (q : L(v2)) n:
+      St v1 = st1 ->
+      St v2 = st2 ->
+      SemSS (exch_sig p q n) st1 st1' ->
+      SemSS (putbuf_sig q n) st2 st2'  ->
+      GSemSS (exch_ev p q n) St (@update_GState (@update_GState St v1 st1') v2 st2').
 
 (* Definition 17 *)
 Inductive Trace G : GState G -> GState G -> Type :=
